9c4a04be39a2003d5d88bea5e4fd6e9dd776ddc3
Harminder2125 <harminder.singh2125@gmail.com>
2023-12-26 10:00:48 +0530

    asd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $allowed_host = env('ALLOWED_DOMAINS');
+       
+        $explode = explode(',',$allowed_host);
+       
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $parseUrl = $_SERVER['HTTP_HOST'];
+           
+            if(!in_array($parseUrl,$explode)) {
+                header($_SERVER['SERVER_PROTOCOL'].' 400 Bad Request');
+                exit;
+            }
+        }  
     }
 
     /**
